04a5bd8908b0a66ba6a7f8e78a8e259c0570a2b3
dbhor <dbhor1000@gmail.com>
2023-06-22 17:06:44 +0300

    Initial commit

---
+++ src/main/java/com/example/currencybottests/CurrencyBotTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencybottests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyBotTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyBotTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/currencybottests/CurrencyBotTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencybottests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyBotTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
